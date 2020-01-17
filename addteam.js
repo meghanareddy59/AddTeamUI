7d6d970ef3ed4e20eccede533b7a80afaaa1d386
meghana <meghana.gaddam@pramati.com>
2020-01-17 12:42:59 +0530

    addteamUI

--- addteam.js
+++ addteam.js
@@ -32,7 +32,15 @@ Button.setAttribute('value','+');
 maindiv.appendChild(Button_div);
 Button_div.appendChild(setname);
 Button_div.appendChild(Button);
-//
+//submit button
+
+var  submitButton = document.createElement('input');
+submitButton.setAttribute('class','submitbutton');
+submitButton.setAttribute('type','submit');
+submitButton.setAttribute('value','submit');
+
+Button_div.appendChild(submitButton);
+
 Button.onclick=function(){
 
     var selectingPlayers_div = document.createElement('div');
@@ -66,27 +74,7 @@ dropdown.appendChild(option3);
 dropdown.appendChild(option4);
 
 
-var dropdown2=document.createElement('select');
-dropdown2.setAttribute('class','dropbox');
-var option_=document.createElement('option');
-option_.textContent="select player";
-var option1_1=document.createElement('option');
-option1_1.textContent="player 1";
-var option2_2=document.createElement('option');
-option2_2.textContent="player 2";
-
-var option3_3=document.createElement('option');
-option3_3.textContent="player 3";
-var option4_4=document.createElement('option');
-option4_4.textContent="player 4";
-
-
-selectingPlayers_div.appendChild(dropdown2);
-dropdown2.appendChild(option_);
-dropdown2.appendChild(option1_1);
-dropdown2.appendChild(option2_2);
-dropdown2.appendChild(option3_3);
-dropdown2.appendChild(option4_4);
+ 
 }
 
 
